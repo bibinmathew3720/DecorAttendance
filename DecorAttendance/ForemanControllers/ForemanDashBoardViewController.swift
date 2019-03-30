60a4ea0eeeaf2ee2f2f36582207e28740709cd2a
Bibin Mathew <bibin.m@codelynks.com>
2019-03-30 07:04:51 +0530

    Foreman dashboard ui change.

--- DecorAttendance/ForemanControllers/ForemanDashBoardViewController.swift
+++ DecorAttendance/ForemanControllers/ForemanDashBoardViewController.swift
@@ -8,23 +8,17 @@
 
 import UIKit
 
-class ForemanDashBoardViewController: UITableViewController, DropDownDataDelegate, MyCAAnimationDelegateProtocol {
+class ForemanDashBoardViewController: UITableViewController, MyCAAnimationDelegateProtocol {
     
     weak var delegate: DashBoardDelegate?
     
     
     @IBOutlet weak var viewAttendanceStatics: UIView!
     @IBOutlet weak var viewDropDownButtons: UIView!
-    @IBOutlet weak var lblSite: UILabel!
     @IBOutlet weak var viewRemainingBonus: UIView!
     @IBOutlet weak var viewPresentAbsent: UIView!
     @IBOutlet weak var pieChartViewLabourSummary: PieChartSliceView!
     
-    @IBOutlet weak var lblDay: UILabel!
-    @IBOutlet weak var lblMonth: UILabel!
-    @IBOutlet weak var tableViewMonth: UITableView!
-    @IBOutlet weak var tableViewDay: UITableView!
-    @IBOutlet weak var tableViewSite: UITableView!
     
     @IBOutlet weak var widthPresentColred: NSLayoutConstraint!
     @IBOutlet weak var widthPresentLight: NSLayoutConstraint!
@@ -40,6 +34,14 @@ class ForemanDashBoardViewController: UITableViewController, DropDownDataDelegat
     @IBOutlet weak var totalAbsenceIndicatorWhite: UIView!
     @IBOutlet weak var totalAbsenceIndicatorColred: UIView!
     
+    @IBOutlet weak var totalEmployeesLabel: UILabel!
+    @IBOutlet weak var presentEmployeesLabel: UILabel!
+    @IBOutlet weak var absentEmployeesLabel: UILabel!
+    @IBOutlet weak var dateView: UIView!
+    @IBOutlet weak var siteView: UIView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var siteLabel: UILabel!
+    
     var window: UIWindow?
     var attendanceSummaryResponse:AttendanceSummaryResponseModel?
     var formanRequest = ForemanAttendanceRequestModel()
@@ -51,7 +53,7 @@ class ForemanDashBoardViewController: UITableViewController, DropDownDataDelegat
         self.setUpChartViewStyles()
         setUpViewStyles()
         pieChartViewLabourSummary.myAnimationDelegate = self
-        addTapGesturesToLabels()
+        addTapGesturesToViews()
         getAttendanceSummaryApi()
         //PieChart.initializeAndPlotGraph(chartView: pieChartView, controller: self, xCoordinateArr: ["23", "45", "32"], yCoordinateArr1: ["23", "45", "32"], yCoordinateArr2: ["23", "45", "32"], yCoordinate1Label: "volume", yCoordinate2Label: "frequency")
         
@@ -65,6 +67,7 @@ class ForemanDashBoardViewController: UITableViewController, DropDownDataDelegat
     }
     
     func initialisation(){
+        self.dateLabel.text = CCUtility.stringFromDate(date: Date())
         self.formanRequest.attendanceDate = CCUtility.stringFromDate(date: Date())
     }
     
@@ -104,7 +107,9 @@ class ForemanDashBoardViewController: UITableViewController, DropDownDataDelegat
     
     func populateAttendanceSummaryResponse(){
         if let attendanceSummary = self.attendanceSummaryResponse{
-            
+           self.totalEmployeesLabel.text = "\(attendanceSummary.total)"
+           self.presentEmployeesLabel.text = "\(attendanceSummary.presentCount)"
+            self.absentEmployeesLabel.text = "\(attendanceSummary.absentCount)"
         }
     }
     
@@ -157,182 +162,53 @@ class ForemanDashBoardViewController: UITableViewController, DropDownDataDelegat
         self.viewPresentAbsent.layer.shadowColor = UIColor(red:0, green:0, blue:0, alpha:0.12).cgColor
         self.viewPresentAbsent.layer.shadowOpacity = 1
         self.viewPresentAbsent.layer.shadowRadius = 9
-        self.lblMonth.layer.cornerRadius = 1
-        self.lblMonth.layer.borderWidth = 0.5
-        self.lblMonth.layer.borderColor = UIColor(red:0.78, green:0.78, blue:0.78, alpha:1).cgColor
         
         
-        self.lblDay.layer.cornerRadius = 1
-        self.lblDay.layer.borderWidth = 0.5
-        self.lblDay.layer.borderColor = UIColor(red:0.78, green:0.78, blue:0.78, alpha:1).cgColor
+//        self.lblDay.textColor = ObeidiFont.Color.obeidiLightBlack()
+//        self.lblMonth.textColor = ObeidiFont.Color.obeidiLightBlack()
         
-        self.lblDay.textColor = ObeidiFont.Color.obeidiLightBlack()
-        self.lblMonth.textColor = ObeidiFont.Color.obeidiLightBlack()
+        self.siteView.layer.cornerRadius = 1
+        self.siteView.layer.borderWidth = 0.5
+        self.siteView.layer.borderColor = UIColor(red:0.78, green:0.78, blue:0.78, alpha:1).cgColor
         
-        addDropDownLabelAndImage(lblToModify: lblMonth, lblText: "Dec")
-        addDropDownLabelAndImage(lblToModify: lblDay, lblText: "22")
+        self.dateView.layer.cornerRadius = 1
+        self.dateView.layer.borderWidth = 0.5
+        self.dateView.layer.borderColor = UIColor(red:0.78, green:0.78, blue:0.78, alpha:1).cgColor
         
+        self.dateLabel.textColor = ObeidiFont.Color.obeidiLightBlack()
+        self.siteLabel.textColor = ObeidiFont.Color.obeidiLightBlack()
+    }
+    
+    func addTapGesturesToViews() {
+        self.dateView.isUserInteractionEnabled = true
+        let tapGestureDate = UITapGestureRecognizer(target: self, action: #selector(ForemanDashBoardViewController.handleDateViewTap))
+        self.dateView.addGestureRecognizer(tapGestureDate)
+        
+        self.siteView.isUserInteractionEnabled = true
+        let tapGesturSite = UITapGestureRecognizer(target: self, action: #selector(ForemanDashBoardViewController.handleSiteViewTap))
+        self.siteView.addGestureRecognizer(tapGesturSite)
+    }
+    
+    @objc func handleDateViewTap(){
         
     }
     
-    func addDropDownLabelAndImage(lblToModify: UILabel, lblText: String) {
-        let image = UIImage(named: "dropdown")
-        let newSize = CGSize(width: 10, height: 10)
-        
-        //Resize image
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
-        image?.draw(in: CGRect(x:0, y:0, width: newSize.width, height: newSize.height))
-        let imageResized = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        //Create attachment text with image
-        let attachment = NSTextAttachment()
-        attachment.image = imageResized
-        let attachmentString = NSAttributedString(attachment: attachment)
-        
-        let myString = NSMutableAttributedString(string: lblText + "      " + "      " + "                 ")
-        myString.append(attachmentString)
-        lblToModify.attributedText = myString
-        
-    }
-    
-    func addTapGesturesToLabels() {
-        
-        self.lblMonth.isUserInteractionEnabled = true
-        let tapGestureMonth = UITapGestureRecognizer(target: self, action: #selector(DashBoardViewController.handleMonthLabelTap))
-        self.lblMonth.addGestureRecognizer(tapGestureMonth)
-        
-        self.lblDay.isUserInteractionEnabled = true
-        let tapGestureDay = UITapGestureRecognizer(target: self, action: #selector(DashBoardViewController.handleDateLabelTap))
-        self.lblDay.addGestureRecognizer(tapGestureDay)
-        
-    }
-    
-    @objc func handleMonthLabelTap(){
-        presentDropDownController(tableCgPoint: getPointForMonthTable(), dropDownFor: .Month, arr: fetchMonthArr())
-    }
-    
-    @objc func handleDateLabelTap(){
-        presentDropDownController(tableCgPoint: getPointForDateTable(), dropDownFor: .Date, arr: fetchDateArr())
-    }
-    
-    func presentDropDownController(tableCgPoint: CGPoint, dropDownFor:
-        DropDownNeededFor, arr: NSMutableArray) {
-        for cell in tableView.visibleCells{
-            
-            cell.backgroundColor = ObeidiFont.Color.obeidiExactBlack()
-            cell.alpha = 0.4
-        }
-        
-        self.navigationController?.navigationBar.alpha = 0.7
-        self.tabBarController?.tabBar.alpha = 0.7
-        
-        
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let dropDownController = storyboard.instantiateViewController(withIdentifier: "DropDownViewControllerID") as! DropDownViewController
-        dropDownController.tableCgPoint = tableCgPoint//CGPoint(x: self.viewDropDownButtons.frame.minX, y: self.viewDropDownButtons.frame.maxY) //+ (self.navigationController?.navigationBar.frame.size.height)!)
-        dropDownController.widthTable = self.lblDay.frame.size.width
-        dropDownController.dropDownNeededFor = dropDownFor
-        dropDownController.delegate = self
-        switch dropDownFor {
-            
-        case DropDownNeededFor.Date:
-            dropDownController.dateArr = arr
-            dropDownController.dropDownNeededFor = dropDownFor
-        case DropDownNeededFor.Month:
-            dropDownController.monthArr = arr
-            dropDownController.dropDownNeededFor = dropDownFor
-        case DropDownNeededFor.Site:
-            dropDownController.siteArr = arr
-            dropDownController.dropDownNeededFor = dropDownFor
-            
-        case .Attendance:
-            dropDownController.dropDownNeededFor = dropDownFor
-        }
-        
-        dropDownController.modalPresentationStyle = UIModalPresentationStyle.overCurrentContext
-        dropDownController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
-        self.present(dropDownController, animated: true, completion: nil)
-        
-        
-    }
-    func fetchDateArr() -> NSMutableArray {
-        
-        let arr = NSMutableArray()
-        for i in 1...31{
-            
-            arr.add(String(i))
-            
-        }
-        return arr
-    }
-    
-    func fetchMonthArr() -> NSMutableArray {
-        
-        var arr = NSMutableArray()
-        arr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        return arr
-    }
-    func fetchSiteArr() -> NSMutableArray {
-        
-        var arr = NSMutableArray()
-        arr = ["Quatar", "Saudi", "Dubai"]
-        return arr
-    }
-    func getPointForMonthTable() -> CGPoint{
-        
-        return CGPoint(x: 12 + self.viewDropDownButtons.frame.minX + self.lblMonth.frame.minX, y: self.viewDropDownButtons.frame.maxY)
-        
-    }
-    func getPointForDateTable() -> CGPoint{
-        
-        return CGPoint(x: 12 + self.viewDropDownButtons.frame.minX + self.lblMonth.frame.size.width + self.lblMonth.frame.minX + 12, y: self.viewDropDownButtons.frame.maxY)
-        
-    }
-    func getPointForSiteTable() -> CGPoint{
-        
-        return CGPoint(x: 12 + self.viewDropDownButtons.frame.minX + self.lblMonth.frame.minX + self.lblDay.frame.size.width + self.lblMonth.frame.size.width + 24, y: self.viewDropDownButtons.frame.maxY)
-        
-    }
-    func changedValue(is value: String!, dropDownType: DropDownNeededFor, index: Int) {
-        
-        switch  dropDownType {
-        case .Date:
-            addDropDownLabelAndImage(lblToModify: self.lblDay, lblText: value)
-        case .Month:
-            addDropDownLabelAndImage(lblToModify: self.lblMonth, lblText: value)
-        case .Site:
-            addDropDownLabelAndImage(lblToModify: self.lblSite, lblText: value)
-            
-        case .Attendance:
-            print("  ")
-        }
-        
-        for cell in tableView.visibleCells{
-            
-            cell.backgroundColor = UIColor.white
-            cell.alpha = 1
-        }
-        
-        self.navigationController?.navigationBar.alpha = 1
-        self.tabBarController?.tabBar.alpha = 1
-        
+    @objc func handleSiteViewTap(){
+       
     }
     
     func setPerformanceIndicatorLines() {
         //This is where the indicators set to their corresponding values
-        
         ObeidiPerformanceIndicatorStyle.setIndicatorsByValues(lineA: self.totalPresenceIndicatorWhite, lineB: totalPresenceIndicatorColored, lineAColor: ObeidiFont.Color.obeidiLineWhite(), lineBColor: ObeidiFont.Color.obeidiLineRed(), lineAValue: 1, lineBValue: 0.18, lineAMeter: widthPresentLight, lineBMeter: widthPresentColred)
         
         ObeidiPerformanceIndicatorStyle.setIndicatorsByValues(lineA: self.totalAbsenceIndicatorWhite, lineB: totalAbsenceIndicatorColred, lineAColor: ObeidiFont.Color.obeidiLineWhite(), lineBColor: ObeidiFont.Color.obeidiLinePink(), lineAValue: 1, lineBValue: 0.98, lineAMeter: widthAbsentLight, lineBMeter: widthAbsentColored)
-        
     }
+    
     func animationDidStop(_ theAnimation: CAAnimation!, finished flag: Bool) {
-        
         if pieChartViewLabourSummary.myAnimationDelegate != nil
         {
             pieChartViewLabourSummary.animating = false
-            pieChartViewLabourSummary.myAnimationDelegate?.animationDidStop( theAnimation, finished: true)
+        pieChartViewLabourSummary.myAnimationDelegate?.animationDidStop( theAnimation, finished: true)
         }
     }
 
