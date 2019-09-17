3876ea5901c9a26fa1dd390b6ea8d19358696697
vendor1.vgsmart@vguard.in <bibin.m@codelynks.com>
2019-09-17 22:15:32 +0530

    Staff view controllers added.

---
+++ DecorAttendance/StaffModule/HomeVC/StaffHomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  StaffHomeVC.swift
+//  DecorAttendance
+//
+//  Created by Bibin Mathew on 9/17/19.
+//  Copyright © 2019 Sreeith n  krishna. All rights reserved.
+//
+
+import UIKit
+
+class StaffHomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
