4cf8edf3c77aeb609618fe79c4e135359cc6b569
vendor1.vgsmart@vguard.in <bibin.m@codelynks.com>
2019-09-17 21:59:52 +0530

    ReportVC adde to project.

---
+++ DecorAttendance/StaffModule/ReportVC/ReportVC.swift
@@ -0,0 +1,30 @@
+//
+//  ReportVC.swift
+//  DecorAttendance
+//
+//  Created by Bibin Mathew on 9/17/19.
+//  Copyright © 2019 Sreeith n  krishna. All rights reserved.
+//
+
+import UIKit
+
+class ReportVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
