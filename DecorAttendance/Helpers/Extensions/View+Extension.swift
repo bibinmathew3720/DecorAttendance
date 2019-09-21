64021efc5f507bdbbe7bd21cfcbd49b57256b633
vendor1.vgsmart@vguard.in <bibin.m@codelynks.com>
2019-09-21 08:45:14 +0530

    Medical Leave vc ui implementation.

---
+++ DecorAttendance/Helpers/Extensions/View+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  View+Extension.swift
+//  DecorAttendance
+//
+//  Created by Bibin Mathew on 9/21/19.
+//  Copyright © 2019 Sreeith n  krishna. All rights reserved.
+//
+
+import UIKit
+
+extension UIView{
+    func setBorderProperties(){
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor(red:0.78, green:0.78, blue:0.78, alpha:1).cgColor
+    }
+}
--- DecorAttendance/StaffModule/MedicalLeaveVC/MedicalLeaveVC.swift
+++ DecorAttendance/StaffModule/MedicalLeaveVC/MedicalLeaveVC.swift
@@ -9,14 +9,47 @@
 import UIKit
 
 class MedicalLeaveVC: UIViewController {
-
+    @IBOutlet weak var startDateView: UIView!
+    @IBOutlet weak var startDateLabel: UILabel!
+    @IBOutlet weak var endDateView: UIView!
+    @IBOutlet weak var endDateLabel: UILabel!
+    @IBOutlet weak var emptyView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        customisation()
 
         // Do any additional setup after loading the view.
     }
     
-
+    func customisation(){
+        startDateView.setBorderProperties()
+        endDateView.setBorderProperties()
+    }
+    
+    //MARK: Button Actions
+    
+    @IBAction func startButtonAction(_ sender: UIButton) {
+        showDatePicker(filterType: .startDate)
+    }
+    
+    @IBAction func endButtonAction(_ sender: UIButton) {
+        showDatePicker(filterType: .endDate)
+    }
+    
+    func showDatePicker(filterType:FilterTypeName){
+        DispatchQueue.main.async {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let calendarViewController = storyboard.instantiateViewController(withIdentifier: "POPUPSelectorViewControllerID") as! POPUPSelectorViewController
+            calendarViewController.delegate = self
+            calendarViewController.filterTypeName = filterType
+            calendarViewController.modalPresentationStyle = UIModalPresentationStyle.overCurrentContext
+            calendarViewController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+            self.present(calendarViewController, animated: true, completion: nil)
+            
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -27,4 +60,28 @@ class MedicalLeaveVC: UIViewController {
     }
     */
 
+}
+
+extension MedicalLeaveVC : filterUpdatedDelegate{
+    func filterValueUpdated(to value: AnyObject!, updatedType: FilterTypeName!) {
+        
+    }
+    
+    func dateUpdated(to date: String, updatedType: FilterTypeName!) {
+        
+    }
+    
+    func calendarColsed() {
+        
+    }
+    
+    func doneButtonActionDelegateWithSelectedDate(date: String, type: FilterTypeName) {
+        
+    }
+    
+    func selectedSite(selSite: ObeidiModelSites, withType: FilterTypeName) {
+        
+    }
+    
+    
 }
